2e0f8d623db2df66abb53d3f53b40c025a593ca2
pledgedavis <pledgedavis14@gmail.com>
2021-01-09 13:49:09 -0500

    added shoes and comments seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shoe.create(name: 'Fire red',  size: '10',  price: '2')
+Shoe.create(name: 'Yeezy1',  size: '9',  price: '560')
+Shoe.create(name: 'Yeezy2',  size: '11',  price: '390')
+Shoe.create(name: 'Yeezy3',  size: '7',  price: '340')
+
+
+Comment.create(description: "Cool", shoe_id: "1")
+Comment.create(description: "Looks good", shoe_id: "2")
